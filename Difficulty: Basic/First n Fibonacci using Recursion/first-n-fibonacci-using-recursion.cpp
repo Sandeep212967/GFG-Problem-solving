d8bbeb95a8bad0d26528fe4e80f524cb3871aa79
SANDEEP KUSHWAHA <sandeepkushwaha959273@gmail.com>
2026-09-22 23:13:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/First n Fibonacci using Recursion/first-n-fibonacci-using-recursion.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    vector<int> fibonacciNumbers(int n) {
+  
+  vector<int> ans;
+
+     if (n >= 1)
+         ans.push_back(0);
+
+     if (n >= 2)
+         ans.push_back(1);
+
+     for (int i = 2; i < n; i++) {
+         ans.push_back(ans[i - 1] + ans[i - 2]);
+     }
+
+     return ans;
+    }
+};
